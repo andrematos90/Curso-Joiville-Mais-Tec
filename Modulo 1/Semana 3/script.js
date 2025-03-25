a1a21d4717a0c87290a2395c9594db34e9365dca
André Matos <andrerosadematos90@gmail.com>
2025-03-24 21:58:51 -0300

    ..

--- Modulo 1/Semana 3/script.js
+++ Modulo 1/Semana 3/script.js
@@ -6,29 +6,40 @@ let nomeFuncionario = 'André Rosa de Matos';
 let salario = 0;
 
 
-if (setor === "GE"){
-    salario = 220 * valorHora;
-}
-else{
-    if(horasTrabalhadas > 220){
-        let horasExecedentes = horasTrabalhadas - 220;
-        salario = 220 * valorHora + (horasExecedentes * (valorHora *2));
-    
-    
-    }else{
-        salario = horasTrabalhadas * valorHora;
+function calcularSalarioBase(setor, horasTrabalhadas, valorHora){
+    if (setor === "GE"){
+        salario = 220 * valorHora;
+    }
+    else{
+        if(horasTrabalhadas > 220){
+            let horasExecedentes = horasTrabalhadas - 220;
+            salario = 220 * valorHora + (horasExecedentes * (valorHora *2));
+        
+        
+        }else{
+            salario = horasTrabalhadas * valorHora;
+        }
+        
     }
     
-}
-
-switch(setor){
+    switch(setor){
+        
+        case "AD":
+            salario = salario + (salario / 100 *10)
+            break;
+        case "GE":
+            salario = salario + (salario / 100 * 25)
+            break
+    }
     
-    case "AD":
-        salario = salario + (salario / 100 *10)
-        break;
-    case "GE":
-        salario = salario + (salario / 100 * 25)
-        break
+    return console.log(salario);
 }
 
-console.log(salario);
+calcularSalarioBase("GE", 250, 9,50);
+
+function buscarValorHora(salarioBase, horasTrabalhadas){
+    return console.log(salarioBase / horasTrabalhadas);
+}
+
+let salarioBase = calcularSalarioBase(salarioBase, horasTrabalhadas, valorHora);
+let valorPorHora = buscarValorHora(salarioBase, horasTrabalhadas);
