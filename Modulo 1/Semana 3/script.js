4ac34f9a351b5083eeb8e17c9ff653cd15c51155
André Matos <andrerosadematos90@gmail.com>
2025-03-24 21:20:02 -0300

    ex 4 ok

--- Modulo 1/Semana 3/script.js
+++ Modulo 1/Semana 3/script.js
@@ -1,17 +1,34 @@
-let setor = 'Desenvolvimento';
+let setor = 'AD';
 let  horasTrabalhadas = 230;
 let valorHora = 9.50;
 let nomeFuncionario = 'André Rosa de Matos';
 
 let salario = 0;
 
-if(horasTrabalhadas > 220){
-    let horasExecedentes = horasTrabalhadas - 220;
-    salario = 220 * valorHora + (horasExecedentes * (valorHora *2));
 
+if (setor === "GE"){
+    salario = 220 * valorHora;
+}
+else{
+    if(horasTrabalhadas > 220){
+        let horasExecedentes = horasTrabalhadas - 220;
+        salario = 220 * valorHora + (horasExecedentes * (valorHora *2));
+    
+    
+    }else{
+        salario = horasTrabalhadas * valorHora;
+    }
+    
+}
 
-}else{
-    salario = horasTrabalhadas * valorHora;
+switch(setor){
+    
+    case "AD":
+        salario = salario + (salario / 100 *10)
+        break;
+    case "GE":
+        salario = salario + (salario / 100 * 25)
+        break
 }
 
 console.log(salario);
