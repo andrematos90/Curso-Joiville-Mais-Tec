54266821bd82d889e0525641008889cc648f43a4
André Matos <andrerosadematos90@gmail.com>
2025-03-31 09:25:49 -0300

    ex 5

--- Modulo 1/Semana 3/script.js
+++ Modulo 1/Semana 3/script.js
@@ -1,45 +1,40 @@
 let setor = 'AD';
-let  horasTrabalhadas = 230;
+let horasTrabalhadas = 230;
 let valorHora = 9.50;
 let nomeFuncionario = 'André Rosa de Matos';
 
-let salario = 0;
+function calcularSalarioBase(setor, horasTrabalhadas, valorHora) {
+    let salario;
 
-
-function calcularSalarioBase(setor, horasTrabalhadas, valorHora){
-    if (setor === "GE"){
+    if (setor === "GE") {
         salario = 220 * valorHora;
-    }
-    else{
-        if(horasTrabalhadas > 220){
-            let horasExecedentes = horasTrabalhadas - 220;
-            salario = 220 * valorHora + (horasExecedentes * (valorHora *2));
-        
-        
-        }else{
+    } else {
+        if (horasTrabalhadas > 220) {
+            let horasExcedentes = horasTrabalhadas - 220;
+            salario = 220 * valorHora + (horasExcedentes * (valorHora * 2));
+        } else {
             salario = horasTrabalhadas * valorHora;
         }
-        
     }
-    
-    switch(setor){
-        
+
+    switch (setor) {
         case "AD":
-            salario = salario + (salario / 100 *10)
+            salario += salario * 0.10; 
             break;
         case "GE":
-            salario = salario + (salario / 100 * 25)
-            break
+            salario += salario * 0.25; 
     }
-    
-    return console.log(salario);
+
+    return salario; 
 }
 
-calcularSalarioBase("GE", 250, 9,50);
-
-function buscarValorHora(salarioBase, horasTrabalhadas){
-    return console.log(salarioBase / horasTrabalhadas);
+function buscarValorHora(salarioBase, horasTrabalhadas) {
+    return salarioBase / horasTrabalhadas;
 }
 
-let salarioBase = calcularSalarioBase(salarioBase, horasTrabalhadas, valorHora);
+
+let salarioBase = calcularSalarioBase(setor, horasTrabalhadas, valorHora);
 let valorPorHora = buscarValorHora(salarioBase, horasTrabalhadas);
+
+console.log("Salário Base:", salarioBase);
+console.log("Valor por Hora:", valorPorHora);
