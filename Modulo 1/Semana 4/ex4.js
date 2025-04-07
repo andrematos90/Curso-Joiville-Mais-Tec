64b741ca92b314e37d90261087a6ad9c4c4e5f6c
André Matos <103394184+andrematos90@users.noreply.github.com>
2025-04-07 09:38:41 -0300

    semana 4 ex4

---
+++ Modulo 1/Semana 4/ex4.js
@@ -0,0 +1,12 @@
+/* Utilize arrow function para criar funções de soma e subtração de dois números.*/
+
+const soma = (a, b) => a + b
+
+const subtracao = (a, b ) =>{
+    return a - b;
+}
+
+console.log(soma(10, 2));
+
+sub = subtracao(10,9);
+console.log(sub);
