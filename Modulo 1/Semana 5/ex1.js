be3a84bba6fbe4c9c32b3c0f20061835ca5bd52d
André Matos <103394184+andrematos90@users.noreply.github.com>
2025-04-09 20:03:09 -0300

    ..

---
+++ Modulo 1/Semana 5/ex1.js
@@ -0,0 +1,2 @@
+/*Inicialmente, criaremos uma tela de login e senha. Essa tela deve possuir o campo de usuário (textual) que será um código de acesso, um campo para escolher um setor (Comercial / RH / TI) e um campo de senha.*/
+
