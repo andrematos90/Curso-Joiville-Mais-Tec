cdfb98f15de4fe346bce5274c28f36a710f1d26f
André Matos <andrerosadematos90@gmail.com>
2025-04-23 20:59:04 -0300

    Semana 6 ex1

---
+++ Modulo 1/Semana 6/ex1.js
@@ -0,0 +1,6 @@
+/*Essa semana, estaremos desenvolvendo um jogo da forca, onde teremos uma palavra que deverá ser encontrada e suas letras ocultas. O jogo funciona basicamente na quantidade de tentativas para acertar a palavra, tendo uma exibição das letras já escolhidas, onde, se a letra contiver na palavra, esta será exibida na palavra; caso não tiver, vai para uma área de letras erradas.
+
+Seu primeiro desafio é a criação do HTML e CSS para essa aplicação, lembrando que temos as entradas de dados sempre feitas pelo usuário por meio de um evento.
+
+Aqui não utilizaremos mais o console.*/
+
