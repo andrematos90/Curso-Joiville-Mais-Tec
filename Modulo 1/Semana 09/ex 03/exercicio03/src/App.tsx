8fdda82208f333071d9b1ce2f5e5f04921460b16
André Matos <andrerosadematos90@gmail.com>
2025-05-19 14:25:14 -0300

    ok

---
+++ Modulo 1/Semana 09/ex 03/exercicio03/src/App.tsx
@@ -0,0 +1,15 @@
+import  Tarefas  from '../src/components/tarefas/Tarefas'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <Tarefas/>
+     
+    </>
+  )
+}
+
+export default App
