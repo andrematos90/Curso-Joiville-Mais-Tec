d6a240094bb90e263a5460b4cd9a39c9dbcfbc5a
André Matos <andrerosadematos90@gmail.com>
2025-06-29 17:28:06 -0300

    modulo2 semena2 ex1

---
+++ Modulo 2/ex1/src/main/java/org/example/Main.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String nomeTomate = "Tomate";
+        double precoTomate = 5.99;
+        int quantidadeTomate = 100;
+
+
+        String nomeBatata = "Batata";
+        double precoBatata = 3.49;
+        int quantidadeBatata = 150;
+
+
+        String nomeCebola = "Cebola";
+        double precoCebola = 4.25;
+        int quantidadeCebola = 80;
+
+      
+        System.out.println("Produto: " + nomeTomate + " | Preço: R$" + precoTomate + " | Quantidade: " + quantidadeTomate);
+        System.out.println("Produto: " + nomeBatata + " | Preço: R$" + precoBatata + " | Quantidade: " + quantidadeBatata);
+        System.out.println("Produto: " + nomeCebola + " | Preço: R$" + precoCebola + " | Quantidade: " + quantidadeCebola);
+    }
+}
